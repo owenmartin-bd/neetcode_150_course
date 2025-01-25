fe13019e4b49be2d596ec9b2b500e34f42fde2f5
Owen Andronicus Martin <owen_0918@LIVE.COM>
2025-01-25 18:34:06 +0600

    Problem-001

---
+++ Problem-001/Problem-001/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Problem_001;
+
+int[] nums = [1, 2, 3, 1];
+
+Console.WriteLine(Solution.ContainsDuplicate(nums)); // True
---
+++ Problem-001/Problem-001/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_001
+{
+    internal class Solution
+    {
+        public static Boolean ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> seenNumbers = [];
+
+            foreach (int num in nums)
+            {
+                if (!seenNumbers.Add(num))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
